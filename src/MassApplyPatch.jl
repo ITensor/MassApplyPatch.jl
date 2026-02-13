4ed30332135a376e20ee71fc66015dbb2b0c9c12
mtfishman <mfishman@flatironinstitute.org>
2026-02-13 15:58:50 -0500

    Cleanup

--- src/MassApplyPatch.jl
+++ src/MassApplyPatch.jl
@@ -7,57 +7,51 @@ if VERSION >= v"1.11.0-DEV.469"
 end
 
 """
-	MassApplyPatch.main(argv)
+    MassApplyPatch.main(argv)
 
 Command line interface for MassApplyPatch.
 
 Arguments:
-- `argv`: Command line arguments. Expected format:
-	massapplypatch <org/repo>... --patch <patchfile> [--branch <branchname>] [--title <prtitle>] [--body <prbody>]
+
+  - `argv`: Command line arguments. Expected format:
+    massapplypatch <org/repo>... --patch <patchfile> [--branch <branchname>] [--title <prtitle>] [--body <prbody>]
 
 The patch function should be provided as a Julia file, which is included and must define a function `patch(repo_path)`.
 """
 function main(argv)
-	repos = String[]
-	patchfile = nothing
-	branchname = "masspatch"
-	prtitle = "Apply mass patch"
-	prbody = "This PR applies a mass patch."
-	i = 1
-	while i <= length(argv)
-		arg = argv[i]
-		if arg == "--patch"
-			i += 1
-			patchfile = argv[i]
-		elseif arg == "--branch"
-			i += 1
-			branchname = argv[i]
-		elseif arg == "--title"
-			i += 1
-			prtitle = argv[i]
-		elseif arg == "--body"
-			i += 1
-			prbody = argv[i]
-		elseif startswith(arg, "--")
-			error("Unknown option: $arg")
-		else
-			push!(repos, arg)
-		end
-		i += 1
-	end
-	if patchfile === nothing
-		error("--patch <patchfile> argument required")
-	end
-	# Load patch function
-	patchfn = nothing
-	Base.include(Main, patchfile)
-	if isdefined(Main, :patch)
-		patchfn = getfield(Main, :patch)
-	else
-		error("Patch file must define a function 'patch(repo_path)'.")
-	end
-	# TODO: Clone each repo, apply patchfn, create PR
-	return nothing
+    repos = String[]
+    patchfile = nothing
+    branchname = "masspatch"
+    prtitle = "Apply mass patch"
+    prbody = "This PR applies a mass patch."
+    for arg in argv
+        if arg == "--patch"
+            patchfile = arg
+        elseif arg == "--branch"
+            branchname = arg
+        elseif arg == "--title"
+            prtitle = arg
+        elseif arg == "--body"
+            prbody = arg
+        elseif startswith(arg, "--")
+            error("Unknown option: $arg")
+        else
+            push!(repos, arg)
+        end
+    end
+    if isnothing(patchfile)
+        error("--patch <patchfile> argument required")
+    end
+    # Load patch function
+    patchfn = nothing
+    Base.include(Main, patchfile)
+    if isdefined(Main, :patch)
+        patchfn = getfield(Main, :patch)
+    else
+        error("Patch file must define a function 'patch(repo_path)'.")
+    end
+    # TODO: Clone each repo, apply patchfn, create PR
+    return nothing
 end
 
 @static if isdefined(Base, Symbol("@main"))
