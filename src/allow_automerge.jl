9cd4c4b3f410c1ca5cec551bd25aff8be41d58f0
mtfishman <mfishman@flatironinstitute.org>
2026-02-19 13:38:32 -0500

    Fix auth

--- src/allow_automerge.jl
+++ src/allow_automerge.jl
@@ -11,11 +11,9 @@ Returns: Dict("OWNER/REPO" => (ok::Bool, status::Int, message::String))
 """
 function allow_automerge!(
         repos::AbstractVector{<:AbstractString}, enable::Bool = true;
-        token::AbstractString = get(ENV, "GITHUB_TOKEN", ""),
         api::GitHub.GitHubAPI = GitHub.DEFAULT_API
     )
-    isempty(token) && error("Set GITHUB_TOKEN or pass `token=`.")
-    auth = GitHub.authenticate(api, token)
+    auth = github_auth()
     results = Dict{String, Tuple{Bool, Int, String}}()
     for repo in repos
         results[repo] = allow_automerge!(repo, enable; api, auth)
